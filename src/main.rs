c8e366c9541b2f515a55e0ca15a37a5aa1505e1c
Ovchinnikov_Stepan <ovstal@gmail.com>
2025-03-18 20:41:16 +0600

    feat: Добавить ввод сложности с консоли
    - Добавлен ввод сложности с консоли с соответсвующим преобразованием и проверками

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,22 @@ mod classes;
 use classes::blockchain::Blockchain;
 
 fn main() {
-    let mut blockchain = Blockchain::new(4);
+    println!("Enter Proof of Work difficulty:");
+
+    let mut pow_difficulty = String::new();
+    io::stdin()
+        .read_line(&mut pow_difficulty)
+        .expect("Unable to read a string");
+
+    let pow_difficulty: usize = match pow_difficulty.trim().parse() {
+        Ok(val) => val,
+        Err(_) => {
+            eprintln!("Error: difficulty must be a number");
+            return;
+        }
+    };
+
+    let mut blockchain = Blockchain::new(pow_difficulty);
 
     blockchain.add_block(String::from("Block 1 Data"));
     blockchain.add_block(String::from("Block 2 Data"));
